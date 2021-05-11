975057a9c4823ae223483156d29862d9f802fa81
rey092 <roman.cheburan@gmail.com>
2021-05-11 20:36:21 +0300

    Added HelloApiView

--- settings/settings.py
+++ settings/settings.py
@@ -15,8 +15,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-APPS_DIR = os.path.join(BASE_DIR, 'src')
-sys.path.insert(0, APPS_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -43,7 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
 
-    'profiles_api',
+    'src.profiles_api',
 ]
 
 MIDDLEWARE = [
